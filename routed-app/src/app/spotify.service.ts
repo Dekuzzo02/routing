81f2821651c8b13219a0fc8dd8a71b89383a837f
Dekuzzo02 <dicuzzo8@gmail.com>
2020-12-11 11:58:38 +0000

    2

--- routed-app/src/app/spotify.service.ts
+++ routed-app/src/app/spotify.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+//Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'  
 })
 export class SpotifyService {
-
- constructor(private http: HttpClient) { }
+  getTrack(trackId: string): any {
+      throw new Error("Method not implemented.");
+  }
+  //url per oauth: https://developer.spotify.com/console/get-search-item/
+  //Ottengo il modulo HttpClient
+  constructor(private http: HttpClient) { }
 
   searchTrack(query: string) {
+    //const url = `https://api.spotify.com/v1/${query}`;
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQC9AQktp1IpMMkNoxdgjCXObew3TxV65bHUBSoExAG1kbSXfKcNAEEuK2VoqXFPggDeudU-a5u3vNfzgZkAPqAEf_RBN7QuXz9JW1JX2pQ7gil0JmXI6S2ot_CXdarqpFl8Blpd6qqRMZrRyRe5-vKAw6T08WY'
+        'Bearer BQCmEHMvjwWcdNY2Q3Y01nunWFTofW5FOdulP_TMRyOJXxWSN_iUa7q9Q8jozznlzoyCKq1zKp7cWYbsHo4AKrTxUjmO5Q_VyonWfClNF2X6GauwWj-SMFtgJsd3wouMm8ZC56CzVsU6KwJRl-pZMRIVudhEtJI'
     });
-
+    
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
- //Ritorno un observable ai componenti che richiedono il servizio
   }
 }
-
